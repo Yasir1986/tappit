1f528e3d353439ddbd8c244c22f12c2699f1b7b4
Yasir1986 <yasir.akbar86@hotmail.com>
2022-01-16 01:15:41 +0000

    table code refactored

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-
 import "./style.css";
 
 const ApiCall = () => {
@@ -17,46 +16,49 @@ const ApiCall = () => {
     fetchMyAPI();
   }, []);
 
-  // return <div>{JSON.stringify(data)}</div>;
+  const renderHeader = () => {
+    let headerElement = [
+      "Name",
+      "Enabled",
+      "Valid",
+      "Authorised",
+      "Palindrome",
+    ];
+
+    return headerElement.map((key, index) => {
+      return <th key={index}>{key.toUpperCase()}</th>;
+    });
+  };
+
+  const renderBody = () => {
+    return data.map(
+      ({ firstName, isEnabled, isValid, isAuthorised, isPalindrome }) => {
+        {
+          console.log(data);
+        }
+        return (
+          <tr>
+            <td>{firstName}</td>
+            <td>{isEnabled ? "true" : "false"}</td>
+            <td>{isValid ? "true" : "false"}</td>
+            <td>{isAuthorised ? "true" : "false"}</td>
+            <td>{isPalindrome ? "true" : "false"}</td>
+          </tr>
+        );
+      }
+    );
+  };
 
   return (
-    <React.Fragment>
-      <div className="wrapper">
-        {console.log(data)}
-        {data.map((data, i) => (
-          <div key={i} className="container">
-            <table>
-              <tr>
-                <th className="table-heading">Name:</th>
-                <td>
-                  {data.firstName} {data.lastName}
-                </td>
-
-                <th className="table-heading">Enabled:</th>
-                <td>{data.isEnabled ? "true" : "false"}</td>
-
-                <th className="table-heading">Valid:</th>
-                <td>{data.isValid ? "true" : "false"}</td>
-
-                <th className="table-heading">Authorised:</th>
-                <td>{data.isAuthorised ? "true" : "false"}</td>
-
-                <th className="table-heading">Palindorme:</th>
-                <td>{data.isPalindorme ? "true" : "false"}</td>
-
-                {data.favouriteSports.map((data, i) => (
-                  <th className="table-heading">
-                    Favorite Sports:
-                    <td>{data.name}</td>
-                  </th>
-                ))}
-              </tr>
-            </table>
-          </div>
-        ))}
-        ;
-      </div>
-    </React.Fragment>
+    <>
+      <h1 id="title">React Table</h1>
+      <table id="employee">
+        <thead>
+          <tr>{renderHeader()}</tr>
+        </thead>
+        <tbody>{renderBody()}</tbody>
+      </table>
+    </>
   );
 };
 
